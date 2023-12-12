4602d843f260086bd098ce3eb14ac2ff5baf9201
ggabrieloliveiraa <gabrieloc.contato@gmail.com>
2023-12-11 23:05:56 -0300

    table images now update live

--- lib/supabase/ImagesService.dart
+++ lib/supabase/ImagesService.dart
@@ -1,20 +1,33 @@
 import 'dart:io';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'dart:typed_data';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ImagesService {
+  final String
+      userId; // Adicione o ID do usuário como uma propriedade da classe
+
+  ImagesService(
+      this.userId); // Modifique o construtor para aceitar o ID do usuário
+
   Future<void> uploadImage(File image) async {
     final storage = Supabase.instance.client.storage;
     final bucket = storage.from('images');
-    // Converter File para Uint8List
-    // Converter File para Uint8List
-    Uint8List fileBytes = await image.readAsBytes();
 
-    final uploadResponse = await bucket.uploadBinary(
-        'fotos${image.path.split('/').last}', fileBytes);
+    Uint8List fileBytes = await image.readAsBytes();
+    String fileName = 'fotos/${image.path.split('/').last}';
+
+    final uploadResponse = await bucket.uploadBinary(fileName, fileBytes);
 
     // URL da imagem carregada
-    final imageUrl = bucket.getPublicUrl('fotos${image.path.split('/').last}');
-    print('URL da Imagem: $imageUrl');
+    final imageUrl = bucket.getPublicUrl(fileName);
+
+    // Inserir registro na tabela de imagens
+    final response = await Supabase.instance.client.from('images').insert({
+      'user_id': userId,
+      'image_url': imageUrl,
+      'uploaded_at': DateTime.now().toIso8601String() // Data e hora do upload
+    });
+
+    print('Imagem carregada e registrada com sucesso! URL: $imageUrl');
   }
 }
